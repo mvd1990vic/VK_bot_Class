c06957458fbb6af8b8bee598139446cd1d9490ea
viktor_LapTop <mvd1990@ya.ru>
2021-02-21 12:10:45 +0700

    small fix

--- bots_class/handlers.py
+++ bots_class/handlers.py
@@ -3,11 +3,13 @@ Handler - обработчики событий разных сценариев.
 
 """
 
+
 def price(event, context):
-    if not 'price' in context:
+    if 'price' not in context:
         context['price'] = 0
-    price = int(int(event.attachments[0]['market']['price']['amount']) / 100)
-    context['price'] += price
+    pricer = int(int(event.attachments[0]['market']['price']['amount']) / 100)
+    context['price'] += pricer
+
 
 def sheets(event, context):
     context['sheets'] = []
